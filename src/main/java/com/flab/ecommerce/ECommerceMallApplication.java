2d4446f79f4bd207f7949e61d40eedb3ad21690c
mjeongriver <jennygh2@hanmail.net>
2025-02-13 18:01:32 +0900

    init: create project

---
+++ src/main/java/com/flab/ecommerce/ECommerceMallApplication.java
@@ -0,0 +1,13 @@
+package com.flab.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/ecommerce/ECommerceMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

d19106379f2cdec6b4d13947957d0c886e1c8858
mjeongriver <mjoengriver@gmail.com>
2025-02-16 01:04:31 +0900

    Feat: 내 정보 조회 API 추가

--- src/main/java/com/flab/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/flab/ecommerce/config/SecurityConfig.java
@@ -2,9 +2,18 @@ package com.flab.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,13 +26,35 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user123@example.com")
+                        .password(passwordEncoder().encode("user123!"))
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        return new ProviderManager(List.of(new DaoAuthenticationProvider() {{
+            setUserDetailsService(userDetailsService);
+            setPasswordEncoder(passwordEncoder());
+        }}));
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users").permitAll()
+                        .requestMatchers("/users", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
+                .httpBasic(withDefaults())
                 .formLogin(withDefaults());
 
         return http.build();

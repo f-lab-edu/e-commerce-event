eb5ee62a2d4491fac0e75b0e6731e6cda06d7392
mjeongriver <mjeongriver@gmail.com>
2025-03-02 21:24:13 +0900

    Refactor: sonarcube 적용 #8

--- src/main/java/com/flab/ecommerce/domain/event/repository/EventRepository.java
+++ src/main/java/com/flab/ecommerce/domain/event/repository/EventRepository.java
@@ -5,8 +5,8 @@ import com.flab.ecommerce.domain.event.enums.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
 import java.util.List;
+
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
